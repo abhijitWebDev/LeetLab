385f3361e350ca01058e3fe5ad2d24a41a059007
Abhijit Mone <110275876+abhijitWebDev@users.noreply.github.com>
2025-06-07 21:20:13 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookiePasser());
 app.use(
   cors({
-    origin:["http://localhost:5173","https://leetlab-production-5d58.up.railway.app"],
+    origin:["http://localhost:5173","https://leetlab-production-5d58.up.railway.app","https://leet-jv2m8mf6k-dhirajs-projects-f59cb973.vercel.app"],
     credentials: true,
   })
 );
